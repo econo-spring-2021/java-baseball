e15f083a6513f27d1b2aac7d3fc4ef39349e1454
Kwon770 <sckwon770@gmail.com>
2021-04-09 21:27:42 +0900

    feat: Specify main methods

--- src/main/java/baseballGame/Main.java
+++ src/main/java/baseballGame/Main.java
@@ -1,6 +1,20 @@
 package baseballGame;
 
 public class Main {
+
+    public static boolean askRetry() {
+        return false;
+    }
+
+    public static void playGame() {
+    }
+
     public static void main(String[] args) {
+        boolean willRetry;
+        do {
+            playGame();
+
+            willRetry = askRetry();
+        } while (willRetry);
     }
 }
