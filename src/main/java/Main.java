4d7ab054b283498fcdcb28deae9912db228c3a0a
asong57 <asong57@naver.com>
2021-04-14 20:23:54 +0900

    FEAT : getHint() 구현

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,17 +15,53 @@ public class Main {
     }
 
     public static void playBaseball(int[] answer) {
+        int input;
         for (int i = 0; i < 9; i++) {
-            getInput();
+            input = getInput();
+            getHint(input, answer);
         }
 
     }
-    public static int getInput(){
+
+    public static int getInput() {
         System.out.print("숫자를 입력해주세요 : ");
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        return number;
+        int input = sc.nextInt();
+        return input;
     }
 
+    public static int[] getHint(int input, int[] answer) {
+        int[] inputs = new int[3];
+        int strike = 0;
+        int ball = 0;
+        for (int i = 2; i >= 0; i--) {
+            inputs[i] = input % 10;
+            input = input / 10;
+        }
+
+        for(int i=0;i<3;i++){
+            if(answer[0] == inputs[i] && i==0){
+                strike++;
+            }else if(answer[0] == inputs[i] && i!=0){
+                ball++;
+            }
+
+            if(answer[1]==inputs[i] && i==1){
+                strike++;
+            }else if(answer[1] == inputs[i] && i!=1){
+                ball++;
+            }
+
+            if(answer[2]==inputs[i] && i==2){
+                strike++;
+            }else if(answer[2] == inputs[i] && i!=2){
+                ball++;
+            }
+        }
+
+        int[] hint = {strike,ball};
+        return hint;
+
+    }
 
 }
