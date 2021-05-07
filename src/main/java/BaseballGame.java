2882ac86f46722733fafe1d30372b8847c86d5ab
JeonginKim <mywnajsldkf@gmail.com>
2021-05-07 13:48:13 +0900

    feat : 컴퓨터가 랜덤으로 3자리 숫자를 생성한다. - 생성한 숫자는 배열에 저장한다.

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class BaseballGame{
     public static void main(String[] args){
         // 여기 안에 코드 작성하는 것
+        Random createRandom = new Random();
+
+        for(int i=0; i < 3; i++){
+            int computerNumber = createRandom.nextInt(9);
+            int[] baseballNumber = new int[3];
+            baseballNumber[i] = computerNumber;
+            System.out.println(baseballNumber[i]);
+        }
+
         for(int i = 1; i <= 9; i++){
             Scanner s = new Scanner(System.in);
             System.out.println("숫자를 입력해주세요 : ");
-
-            double baseballNumber = s.nextInt();
+            double userInput = s.nextInt();
         }
+
+        int[] userNumber = new int[3];
+
+        System.out.println("9번의 시도 동안 맞히지 못했습니다! 당신의 패배입니다.\n");
     }
 }
