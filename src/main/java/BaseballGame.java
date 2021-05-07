04079ed96e4b9d34a2449451645bc960695a5cd8
JeonginKim <mywnajsldkf@gmail.com>
2021-05-07 13:19:09 +0900

    feat : 숫자 입력

---
+++ src/main/java/BaseballGame.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BaseballGame{
+    public static void main(String[] args){
+        // 여기 안에 코드 작성하는 것
+        for(int i = 1; i <= 9; i++){
+            Scanner s = new Scanner(System.in);
+            System.out.println("숫자를 입력해주세요 : ");
+
+            double baseballNumber = s.nextInt();
+        }
+    }
+}
